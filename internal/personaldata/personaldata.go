07628f99d57884c1e1f58798645addb5ec39732e
vgdryukov <vgdryukov@gmail.com>
2025-10-22 23:02:57 +0300

    pre-release 02

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -12,6 +12,6 @@ type Personal struct {
 func (p Personal) Print() {
 	// TODO: реализовать функцию
 	fmt.Printf("Имя: %s\n", p.Name)
-	fmt.Printf("Вес: %.2f\n", p.Weight)
-	fmt.Printf("Рост: %.2f\n", p.Height)
+	fmt.Printf("Вес: %.2f кг.\n", p.Weight)
+	fmt.Printf("Рост: %.2f м.\n", p.Height)
 }
